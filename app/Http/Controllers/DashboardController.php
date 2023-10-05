41299794be4357d4dff58986cbb8a96a10cb3880
SKS IT FIRM <sksitfirm@gmail.com>
2023-10-05 09:24:50 +0600

    remove breeze

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,36 +10,10 @@ use Illuminate\Support\Facades\{DB,Cache};
 class DashboardController extends Controller
 {
     public function dashboard(){
-        
-        //assign role 
-        // $user = \Auth::user();
-        // dd($user);
-        // if($user->hasRole('user')==false){
-        //     $role = Role::where('slug','user')->first();
-        //     $user->roles()->attach($role);
-        // }
-        
-        // check role
-        //dd($user->hasRole('admin'));
-        
-        
-        // 
-        // assign permission 
-        // $permission = Permission::all();
-        // $user->permissions()->attach($permission);
-        // dd($user->permissions);
-        
-        // check permission
-        // dd($user->hasPermission('add-post'));
-        //dd($user->can('add-post'));
-        // 
-        // 
-        // 
-        // 
-        //dd($user->roles);
-        //dd($user->permissions);
-        
-        $users = Cache::remember('users', now()->addMinutes(60*24), function () { return DB::table('users')->select('id', 'name', 'email')->get(); });
+    
+        $users = Cache::remember('users', now()->addMinutes(60*24), function () { 
+            return DB::table('users')->select('id', 'name', 'email')->get(); 
+        });
         //$users = DB::table('users')->select(['id', 'name', 'email'])->get();
         return view('dashboard', compact('users'));
     }
